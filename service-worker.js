13574b99223644fad40c5f1e3519f07221dcbb9c
2ndobticarlos-art <2ndobticarlos@gmail.com>
2026-07-29 10:29:01 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'bus-tracker-cde-v1';
+const urlsToCache = [
+  '/bus-tracker-cde/',
+  '/bus-tracker-cde/index.html'
+];
+
+// Instalar el service worker y cachear archivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activar y limpiar caches viejos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Responder con cache si no hay internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
